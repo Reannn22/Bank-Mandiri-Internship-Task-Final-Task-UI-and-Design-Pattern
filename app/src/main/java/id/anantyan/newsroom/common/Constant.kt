7b422de7baf9fff08537083413a4e09580607611
Reyhan Capri Moraga <reyhan.123140022@student.itera.ac.id>
2025-03-31 22:38:46 +0700

    Update Constant.kt

--- app/src/main/java/id/anantyan/newsroom/common/Constant.kt
+++ app/src/main/java/id/anantyan/newsroom/common/Constant.kt
@@ -2,5 +2,5 @@ package id.anantyan.newsroom.common
 
 object Constant {
     const val BASE_URL = "https://newsapi.org/v2/"
-    const val API_KEY = "f77ada5ee5ac4734935225b405c7e650"
+    const val API_KEY = ""
 }
